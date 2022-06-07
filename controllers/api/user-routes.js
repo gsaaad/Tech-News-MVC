79e63e688c3c957f2379b9bc5a2c848ed59c1142
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-06-07 12:41:34 -0400

    added functionality to add votes and comments, and remove form when user is not logged in

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -59,20 +59,24 @@ router.post("/", (req, res) => {
     username: req.body.username,
     email: req.body.email,
     password: req.body.password,
-  }).then((dbUserData) => {
-    // this methid will initiate the creation of the session, then run the callback function once complete
-    req.session.save(() => {
-      req.session.user_id = dbUserData.id;
-      req.session.username = dbUserData.username;
-      req.session.loggedIn = true;
+  })
+    .then((dbUserData) => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
 
-      res.json(dbUserData);
+        res.json(dbUserData);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-  });
 });
 
-// login and logout
 router.post("/login", (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
       email: req.body.email,
@@ -91,7 +95,6 @@ router.post("/login", (req, res) => {
     }
 
     req.session.save(() => {
-      // declare session variables
       req.session.user_id = dbUserData.id;
       req.session.username = dbUserData.username;
       req.session.loggedIn = true;
@@ -100,15 +103,17 @@ router.post("/login", (req, res) => {
     });
   });
 });
-// router.post("/logout", (req, res) => {
-//   if (req.session.loggedIn) {
-//     req.session.destroy(() => {
-//       res.status(204).end();
-//     });
-//   } else {
-//     res.status(404).end();
-//   }
-// });
+
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
 router.put("/:id", (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
@@ -152,34 +157,3 @@ router.delete("/:id", (req, res) => {
 });
 
 module.exports = router;
-// !old code
-//    ! .then(dbUserData => {
-//     !  res.json(dbUserData);
-//!     })
-//!     .catch(err => {
-//!       console.log(err);
-//!       res.status(500).json(err);
-//!     });
-
-// !router.post("/login", (req, res) => {
-//   // expects {email: 'lernantino@gmail.com', password: 'password1234'}
-//   User.findOne({
-//     where: {
-//       email: req.body.email,
-//     },
-//   }).then((dbUserData) => {
-//     if (!dbUserData) {
-//       res.status(400).json({ message: "No user with that email address!" });
-//       return;
-//     }
-
-//     const validPassword = dbUserData.checkPassword(req.body.password);
-
-//     if (!validPassword) {
-//       res.status(400).json({ message: "Incorrect password!" });
-//       return;
-//     }
-
-//     res.json({ user: dbUserData, message: "You are now logged in!" });
-//   });
-// });
